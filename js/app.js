cee351d0bb593ba48a28504e7383d78b59a36646
Dominika Szukała <dominika.szukala@gmail.com>
2018-11-29 22:58:09 +0100

    Exercise with jQuery

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-	console.log('ready')
+	$(".main").prepend("<h1 class='title'>SAMPLE INTERESTING PAGE</h1>");
+	$(".title").css("color", "teal");
+	$("h3:eq(0)").css("color", "pink");
+
+	var text = $("<p>");
+	text.append("<h2>Elo elo 3, 2 0!");
+
+	$(".main").append(text);
+
+	whenEnter = () => {
+		$(".main").css("background-color", "pink");
+	}
+
+	whenLeave = () => {
+		$(".main").css("background-color", "beige");
+	}
+
+	$(".clickme").on("click", whenEnter);
+	$(".clickme").on("mouseleave", whenLeave);
 });
